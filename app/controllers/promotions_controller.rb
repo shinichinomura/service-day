98726954cfd2746a9fa5a3049f70395cd52e6ac0
Shinichi NOMURA <shinichi.nomura@gmail.com>
2017-12-02 21:55:36 +0900

    プロモーション詳細画面をAMP対応

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -3,5 +3,10 @@ class PromotionsController < ApplicationController
     @promotion = Promotion.find(params[:id])
 
     @page_title = "#{@promotion.title}（#{@promotion.shop.name}）"
+
+    if params[:amp]
+      render 'show-amp', layout: 'application-amp'
+    else
+    end
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,11 @@ module ApplicationHelper
   def title(page_title: "飲食店や映画館をおトクに利用しよう")
     "サービスデイ - #{page_title}"
   end
+
+  def render_css(path)
+    raw Sass::Engine.for_file("#{Rails.root}/app/assets/stylesheets/#{path}", {
+      load_paths: ["#{Rails.root}/app/assets/stylesheets"],
+        style: :compressed
+    }).to_css
+  end
 end
